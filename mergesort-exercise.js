c9c46988502151c06656c3b69f6c8001840fa937
Daniel Lee <d.lee084@gmail.com>
2024-09-06 10:27:20 -0700

    added solutions to mergesort-exercise.js

--- mergesort-exercise.js
+++ mergesort-exercise.js
@@ -1,26 +1,53 @@
 // TASK: Implement mergesort!
-// mergeSort(list) psuedocode
-//     base case: if list.length < 2, return
-//     break the list into halves L & R
-//     Lsorted = mergeSort(L)
-//     Rsorted = mergeSort(R)
-//     return merge(Lsorted, Rsorted)
-// initialize n to the length of the list
-// base case is if n < 2, just return
-// initialize mid to n/2
-// left = left slice of array to mid - 1
-// right = right slice of array to mid to n - 1
-// mergeSort(left)
-// mergeSort(right)
-// merge(left, right, a)
-function mergeSortExercise(list) {
-    if (list.length < 2) {
-        return list
-    } else {
-        let mid = Math.floor(list.length / 2)
 
-        console.log(mid, 'test')
-        return list
+// Split array into halves and merge them recursively
+// mergeSort(list) psuedocode
+    // base case: if list.length < 2, return
+    // break the list into halves L & R
+    // Lsorted = mergeSort(L)
+    // Rsorted = mergeSort(R)
+    // return merge(Lsorted, Rsorted)
+function mergeSort(arr) {
+    // base case
+    if (arr.length === 1) {
+        // return once we hit an array with a single item --which is sorted
+        return arr
     }
+
+    const middle = Math.floor(arr.length - 1 / 2) // get middle item of the array round down (e.g. 5/2 -> 2)
+    const left = arr.slice(0, middle) // items on the left side
+    const right = arr.slice(middle) // items on the right side
+    const sortedLeft = mergeSort(left)
+    const sortedRight = mergeSort(right)
+    // recursively merge left and right
+    return merge(sortedLeft, sortedRight)
 }
-// console.log(mergeSortExercise([5,1,4,2,8]))
+
+// compare the arrays item by item and return the concatenated result
+// initialize empty array
+// compare the first index of the left array to the first index of the right array
+// push the lower value to the empty array
+// shift the array with the lower value
+// repeat until both L and R arrays are empty
+function merge(leftArr, rightArr) {
+    let result = [];
+    let indexLeft = 0;
+    let indexRight = 0;
+
+    while (indexLeft < leftArr.length && indexRight < rightArr.length) {
+        if (leftArr[indexLeft] < rightArr[indexRight]) {
+            result.push(leftArr[indexLeft])
+            indexLeft++
+        } else {
+            result.push(rightArr[indexRight])
+            indexRight++
+        }
+    }
+
+    return result.concat(leftArr.slice(indexLeft)).concat(rightArr.slice(indexRight))
+}
+
+const list = [2,5,1,3,7,2,3,8,6,3]
+
+console.log(mergeSort([5,1,4,2,8]))
+console.log(mergeSort(list))
